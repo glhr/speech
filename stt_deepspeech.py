d578a83dd55a0472342b005274d8fe46f4c4b996
glhr <grenblot@gmail.com>
2020-03-19 13:37:22 +0100

    moved audio I/O methods to utils.py

--- stt_deepspeech.py
+++ stt_deepspeech.py
@@ -1,11 +1,13 @@
 import pyaudio
 import deepspeech
 from deepspeech import Model
-import scipy.io.wavfile as wav
-import speech_recognition as sr
+# import scipy.io.wavfile as wav
+# import speech_recognition as sr
+import utils
 
 from pathlib import Path
 dirpath = str(Path(__file__).parent.absolute())
+
 
 def load_deepspeech_model():
     N_FEATURES = 25
@@ -17,22 +19,20 @@ def load_deepspeech_model():
     ds = Model(dirpath + '/deepspeech-0.6.1-models/output_graph.pbmm', BEAM_WIDTH)
     return ds
 
+
 ds = load_deepspeech_model()
+
 
 def deepspeech(audio):
     return ds.stt(audio)
 
+
 def generate_text(audio, method='deepspeech'):
     return deepspeech(audio)
 
+
 if __name__ == "__main__":
-    r = sr.Recognizer()
-    with sr.Microphone(sample_rate=48000,chunk_size=2048) as source:
-        # Adjusts the energy threshold dynamically using audio from source (an AudioSource instance) to account for ambient noise.
-        print("Please wait one second for calibrating microphone...")
-        r.adjust_for_ambient_noise(source,duration=1)
-        print("Ok, I am ready...")
-        r.dynamic_energy_threshold = True
-        audio = r.listen(source)
-        text = generate_text(audio, method='deepspeech')
-        print(text)
+    r, audio = utils.record_audio()
+    wav = utils.load_audio()
+    text = generate_text(wav, method='deepspeech')
+    print(text)
---
+++ utils.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import scipy.io.wavfile as wav
+
+r = sr.Recognizer()
+
+
+def record_audio():
+    with sr.Microphone(sample_rate=48000, chunk_size=2048) as source:
+        # Adjusts the energy threshold dynamically using audio from source (an AudioSource instance) to account for ambient noise.
+        print("Please wait one second for calibrating microphone...")
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("Ok, I am ready...")
+        r.dynamic_energy_threshold = True
+        audio = r.listen(source)
+        with open("output.wav", "wb") as file:
+            file.write(audio.get_wav_data())
+        return r, audio
+
+def load_audio(filename="output.wav"):
+    _, audio = wav.read(filename)
+    return audio
