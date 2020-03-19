4a1d1c3b3b69961f72adf7b2484e5ad72f7f5690
glhr <grenblot@gmail.com>
2020-03-19 15:02:49 +0100

    read STT method from sys args

--- stt_wrapper.py
+++ stt_wrapper.py
@@ -2,11 +2,15 @@ def generate_text(filename, method='google'):
     if method == 'deepspeech':
         from stt_deepspeech import stt_deepspeech
         return stt_deepspeech(filename)
-    if method == 'google':
+    elif method == 'google':
         from stt_google import stt_google
         return stt_google(filename)
+    else:
+        print("Unknown Speech-to-Text method provided")
 
 
 if __name__ == "__main__":
-    text = generate_text(filename="output.wav")
+    import sys
+    method = sys.argv[1] if len(sys.argv)>1 else 'google'
+    text = generate_text(filename="output.wav", method=method)
     print(text)
