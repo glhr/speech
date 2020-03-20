ed20cda546e3720e9621fb8b99b12ffdbd22e4d2
glhr <grenblot@gmail.com>
2020-03-20 12:48:36 +0100

    count number of incorrect words in STT output

--- comparing_stt.py
+++ comparing_stt.py
@@ -5,15 +5,22 @@ import json
 
 logger = utils.get_logger()
 # list of supported speech recognition methods
-methods = ['google','deepspeech']
+methods = ['google']
 
 with open('audio/dataset.json') as f:
     data = json.load(f)['data']
     for recording in data:
-        logger.debug("{} ({})".format(recording['phrase'.lower()], recording['file']))
+        logger.debug("{} ({})".format(recording['phrase'].lower(), recording['file']))
         for method in methods:
             try:
                 text = generate_text(recording['file'], method=method)
+
+                # compute ratio of correct words
+                expected = recording['phrase'].lower().split()
+                output = text.split()
+                diff = [word for word in expected if word not in output]
+
                 logger.info("--> {}: {}".format(method, text))
+                logger.info("----> {} incorrect word(s) ".format(len(diff), diff))
             except Exception as e:
                 logger.error(e)
