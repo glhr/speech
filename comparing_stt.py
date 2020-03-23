b2b1fd1bb48872b6850373eb027f1e82d266957e
glhr <grenblot@gmail.com>
2020-03-23 16:58:56 +0100

    calculate total inference time for each method

--- comparing_stt.py
+++ comparing_stt.py
@@ -2,13 +2,16 @@ from stt_wrapper import generate_text
 import utils
 import json
 import pandas as pd
+import numpy as np
+from timeit import default_timer as timer
 
 logger = utils.get_logger()
 # list of supported speech recognition methods
-methods = ['google', 'wit', 'sphinx', 'deepspeech', 'houndify']
+methods = ['deepspeech', 'google', 'wit', 'sphinx', 'houndify']
 # methods = ['google']
 
 results = {}
+timings = {}
 
 with open('audio/dataset.json') as f:
     data = json.load(f)['data']
@@ -18,14 +21,18 @@ with open('audio/dataset.json') as f:
         logger.debug("{} ({})".format(expected, recording['file']))
         for method in methods:
             try:
+                inference_start = timer()
                 output = generate_text('resampled/'+recording['file'], method=method)
+                inference_end = timer() - inference_start
                 # diff = [word for word in expected if word not in output]
                 try:
                     results[method][expected] = output
+                    timings[method].append(inference_end)
                 except KeyError:
                     results[method] = {expected: output}
+                    timings[method] = [inference_end]
 
-                logger.info("--> {}: {}".format(method, output))
+                logger.info("--> {}: {} ({:.3f} s)".format(method, output, inference_end))
                 # logger.warn("----> {} incorrect word(s) ".format(len(diff), diff))
             except Exception as e:
                 logger.error(e)
@@ -37,6 +44,7 @@ for method in methods:
     metrics = utils.evaluate_results(results[method].keys(), results[method].values())
     utils.reset_eval_variables()
     metrics['method'] = method
+    metrics['time'] = np.sum(timings[method])
     metrics_full.append(metrics)
 
 df = pd.DataFrame(metrics_full)
